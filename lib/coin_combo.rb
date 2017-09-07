001df6bf82498e682ebd3f2c1944e4d7946347a9
Margaret Berry and Sara Spink <margaret@>
2017-09-07 10:33:08 -0700

    create a function to add two numbers and pass a test on that

---
+++ lib/coin_combo.rb
@@ -0,0 +1,16 @@
+#! usr/bin/env ruby
+class Money
+  # def initialize (myNum)
+  #   @myNum = myNum
+  # end
+  def coin_combo()
+    1+4
+    # return 25
+    # @myNum
+    # my_array = [@myNum]
+    # my_array.map do |myNum|
+    # Float((myNum)*(0.01))
+  end
+    # return my_array[0]
+  #  end
+end
---
+++ spec/coin_combo_spec.rb
@@ -0,0 +1,11 @@
+require ('rspec')
+require ('pry')
+require ('coin_combo')
+
+newmoney = Money.new()
+
+describe('#coin_combo') do
+  it ("take a number and return it as cents") do
+    expect(newmoney.coin_combo()).to(eq(5))
+  end
+end
